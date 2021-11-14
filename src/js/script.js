3334737afa5b62978e5119e1605d0ec4e33085fc
Rémy Pelletier <remypelletier2@gmail.com>
2021-11-14 14:30:50 +0100

    feat: add style for mobile device

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('loaded');
+
+    document.getElementById('navbarMenuIcon').addEventListener('click', (e) => {
+        e.currentTarget.classList.toggle('toggle');
+    });
 });
