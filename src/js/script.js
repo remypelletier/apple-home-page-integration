1d632d48ed6952bf4807c71cca82e72e480931c3
Rémy Pelletier <remypelletier2@gmail.com>
2021-11-14 22:30:12 +0100

    feat: add mobile header animations on open

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('loaded');
 
     document.getElementById('navbarMenuIcon').addEventListener('click', (e) => {
-        e.currentTarget.classList.toggle('toggle');
+        e.currentTarget.parentNode.parentNode.classList.toggle('menu-open');
     });
 });
